98290ef91e008e823d5918649c3d1afd76cb386e
jahroost <claysmart@gmail.com>
2017-10-18 12:32:26 -0500

    created react component

--- app.js
+++ app.js
@@ -1,1 +1,15 @@
-// TODO
+var GroceryListItem = (props) => (
+  <li>{props.item}</li>
+)
+
+var GroceryList = (props) => (
+  <div>
+    <h2>Grocery List</h2>
+    <ul>
+      {props.groceries.map(item => <GroceryListItem item={item} />)}
+    </ul>
+  </div>
+)
+
+
+ReactDOM.render(<GroceryList groceries={['milk', 'sugar']}/>, document.getElementById('app'))
